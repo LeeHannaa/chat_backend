1bebf854420ac1e381b3195663b92e4193b9ecc4
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2025-03-17 15:25:55 +0900

    fix: 웹소켓 연결 오류 해결

--- src/main/java/com/ddhouse/chat/config/WebSocketBrokerConfig.java
+++ src/main/java/com/ddhouse/chat/config/WebSocketBrokerConfig.java
@@ -1,6 +1,5 @@
 package com.ddhouse.chat.config;
 
-import com.ddhouse.chat.handler.WebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -10,16 +9,29 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) { // STOMP에서 사용하는 메시지 브로커를 설정
-        registry.enableSimpleBroker("/sub"); // 메시지를 구독(수신)하는 요청 엔드포인트
-        registry.setApplicationDestinationPrefixes("/pub"); // 메시지를 발행(송신)하는 엔드포인트
-    }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint( "/chatting") // 초기 핸드셰이크에서 사용할 endpoint
-                .setAllowedOrigins("*")
-                .withSockJS();
-    }
+ @Override
+ public void registerStompEndpoints(StompEndpointRegistry registry) {
+  registry.addEndpoint("/ws-stomp")
+          .setAllowedOrigins("*"); // 보안 강화
+//          .setAllowedOriginPatterns("*")
+//          .withSockJS();
+
+  //Client에서 websocket 연결할 때 사용할 API 경로를 설정 - 채팅용
+//  registry.addEndpoint("/chat")
+//          .setAllowedOriginPatterns("*")
+//          .withSockJS();
+  //Client에서 websocket 연결할 때 사용할 API 경로를 설정 - 매칭용
+//  registry.addEndpoint("/match")
+//          .setAllowedOriginPatterns("*")
+//          .withSockJS();
+
+
+ }
+
+ @Override
+ public void configureMessageBroker(MessageBrokerRegistry registry) {
+  registry.enableSimpleBroker("/topic"); // 구독할 경로 설정
+  registry.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 보낼 경로
+ }
 }
